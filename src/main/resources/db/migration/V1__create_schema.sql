f3b6841408ed1b17f2f96dcb6b2abe0efaf813b0
Łukasz Plewnia <plelukas@gmail.com>
2019-05-24 23:02:29 +0200

    more modifications to schema

--- src/main/resources/db/migration/V1__create_schema.sql
+++ src/main/resources/db/migration/V1__create_schema.sql
@@ -1,28 +1,8 @@
 
-CREATE TABLE events
-(
-    id INTEGER PRIMARY KEY,
-    date DATE,
-    count BIGINT,
-    articles_number INTEGER
-);
-
 CREATE TABLE organizations
 (
     id INTEGER PRIMARY KEY,
     name VARCHAR(255)
-);
-
-CREATE TABLE sources
-(
-    id INTEGER PRIMARY KEY,
-    base_url VARCHAR(255)
-);
-
-CREATE TABLE source_urls
-(
-    id INTEGER PRIMARY KEY,
-    exact_url VARCHAR(1000)
 );
 
 CREATE TABLE types
@@ -32,10 +12,37 @@ CREATE TABLE types
 
 );
 
+CREATE TABLE sources
+(
+    id INTEGER PRIMARY KEY,
+    base_url VARCHAR(255)
+);
+
+CREATE TABLE source_event
+(
+    id INTEGER PRIMARY KEY,
+    source_id INTEGER REFERENCES sources(id),
+    event_id INTEGER REFERENCES events(id)
+);
+
+CREATE TABLE source_urls
+(
+    id INTEGER PRIMARY KEY,
+    source_id INTEGER REFERENCES sources(id),
+    exact_url VARCHAR(1000)
+);
+
 CREATE TABLE persons
 (
     id INTEGER PRIMARY KEY,
     name VARCHAR(255)
+);
+
+CREATE TABLE person_event
+(
+    id INTEGER PRIMARY KEY,
+    person_id INTEGER REFERENCES persons(id),
+    event_id INTEGER REFERENCES events(id)
 );
 
 CREATE TABLE locations
@@ -48,7 +55,33 @@ CREATE TABLE locations
     longitude FLOAT
 );
 
+CREATE TABLE location_event
+(
+    id INTEGER PRIMARY KEY,
+    location_id INTEGER REFERENCES locations(id),
+    event_id INTEGER REFERENCES events(id)
+);
+
 CREATE TABLE cameo_events
 (
     id INTEGER PRIMARY KEY
-)
+);
+
+CREATE TABLE events
+(
+    id INTEGER PRIMARY KEY,
+    date DATE,
+    count BIGINT,
+    articles_number INTEGER,
+
+    -- many-to-one
+    organization_id INTEGER REFERENCES organizations(id),
+    type_id INTEGER REFERENCES types(id)
+
+    /*
+    many-to-many:
+    - source_event
+    - person_event
+    - location_event
+     */
+);
