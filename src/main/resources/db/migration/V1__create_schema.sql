92188ee8ab446f2739b78c6ca2ce49c31b860109
Łukasz Plewnia <plelukas@gmail.com>
2019-05-24 22:50:57 +0200

    modify db schema

--- src/main/resources/db/migration/V1__create_schema.sql
+++ src/main/resources/db/migration/V1__create_schema.sql
@@ -1,32 +1,51 @@
 
 CREATE TABLE events
 (
-    id INTEGER PRIMARY KEY
+    id INTEGER PRIMARY KEY,
+    date DATE,
+    count BIGINT,
+    articles_number INTEGER
 );
 
 CREATE TABLE organizations
 (
-    id INTEGER PRIMARY KEY
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255)
 );
 
 CREATE TABLE sources
 (
-    id INTEGER PRIMARY KEY
+    id INTEGER PRIMARY KEY,
+    base_url VARCHAR(255)
+);
+
+CREATE TABLE source_urls
+(
+    id INTEGER PRIMARY KEY,
+    exact_url VARCHAR(1000)
 );
 
 CREATE TABLE types
 (
-    id INTEGER PRIMARY KEY
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255)
+
 );
 
 CREATE TABLE persons
 (
-    id INTEGER PRIMARY KEY
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255)
 );
 
 CREATE TABLE locations
 (
-    id INTEGER PRIMARY KEY
+    id INTEGER PRIMARY KEY,
+    country_code VARCHAR(255),
+    country VARCHAR(255),
+    city VARCHAR(255),
+    latitude FLOAT,
+    longitude FLOAT
 );
 
 CREATE TABLE cameo_events
